7632a38b98be7fc8758e92230b50c85c4412bc97
chris-m92 <chrismcbride92@gmail.com>
2021-11-05 21:57:14 -0400

    1.2.2 - Transpile TSX

--- lib/Control.d.ts
+++ lib/Control.d.ts
@@ -3,6 +3,7 @@ import L from 'leaflet';
 interface Props {
     position: L.ControlPosition;
     children?: React.ReactNode;
+    style?: React.CSSProperties;
 }
 declare const Control: (props: Props) => JSX.Element;
 export default Control;
